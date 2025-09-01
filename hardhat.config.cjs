b90c8e33dd7d6618e8a960ad429cefc823db16d4
ssebattasamuel <ssebattasamuel@gmail.com>
2025-09-01 20:08:28 +0300

    Updates: Added dotenv for secure keys, chain switch in frontend, Holesky config, and deployment fixes

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,17 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    holesky: {
+      url: "https://eth-holesky.g.alchemy.com/v2/-aQQta2KHRN-wz0EUujVL",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: "YTZ7IJSYB1DW278TVNB4U3NWW9YNCWGUDR",
+  },
 };
+console.log("Private key:", process.env.PRIVATE_KEY);
